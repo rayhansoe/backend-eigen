7397c8ff6dfde0aa534b76a35243e5724b77f60e
bukanrayhan <rayhan.rhssoe2@gmail.com>
2022-07-12 17:09:22 +0700

    add register and getUsers for user Routes.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,14 +2,17 @@ const express = require('express')
 const router = express.Router()
 
 const {
+	registerUser,
 	loginUser,
 	getUserProfile,
 	logout,
 	handleRefreshToken,
+	getUsers
 } = require('../controllers/userController')
 
 const { protectGetUser, protect } = require('../middleware/authMiddleware')
 
+router.route('/').post(registerUser).get(getUsers)
 router.route('/login').post(loginUser)
 router.route('/logout').delete(protect, logout)
 router.route('/refreshToken').get(handleRefreshToken)
