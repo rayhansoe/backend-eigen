af6e0620a2174ac095434a466f3b922ecb27905f
bukanrayhan <rayhan.rhssoe2@gmail.com>
2022-07-14 06:07:05 +0700

    update penalize condition middleware.

--- middleware/userMiddleware.js
+++ middleware/userMiddleware.js
@@ -2,9 +2,16 @@ const asyncHandler = require('express-async-handler')
 
 const protectUser = asyncHandler(async (req, res, next) => {
 	const books = req?.user?.books
+	const isPenalized = req?.user?.penalty
+
+	if (isPenalized) {
+		res.status(403)
+		throw new Error('You cannot borrow this book because you are penalized.')
+	}
+
 	if (books && books.length >= 2) {
 		res.status(403)
-		throw new Error("You can't borrow this book because you already borrowed 2 other books")
+		throw new Error("You can't borrow this book because you already borrowed 2 other books.")
 	}
 	next()
 })
