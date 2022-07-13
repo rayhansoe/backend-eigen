f4b4d5c81293560bbcea136d7a97b8710d19d235
bukanrayhan <rayhan.rhssoe2@gmail.com>
2022-07-13 23:21:43 +0700

    add & delete cooment.

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -3,22 +3,7 @@ const router = express.Router()
 
 const { setBook, getBooks, getBookByParams } = require('../controllers/bookController')
 
-// Book Routes
-// Read All Books
-// Read Available Books
-// Pinjam Book
-// Balikin Buku
-
-// Functions
-// getAllBooks
-// getAvailBooks
-// borrowBook
-// balikinBook
-
-router.route('/').get(getBooks).post(setBook) // Set Book
-router.route('/:params').get(getBookByParams) // Get Available Books
-// router.route('/:id').get(getBookByID) // Get Unavailable Books
-// router.route('/') // Pinjam Books | PUT or POST ?
-// router.route('/') // Balikin Books | PUT or POST ?
+router.route('/').get(getBooks).post(setBook) // Set Book | Get Books
+router.route('/:params').get(getBookByParams) // Get Detail Book
 
 module.exports = router
