f4324211d594c408d0d7a395b2304e413eaf5dad
bukanrayhan <rayhan.rhssoe2@gmail.com>
2022-07-14 16:26:52 +0700

    add semiProtected and protect middlware in bookRoutes.

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
+const { semiProtected, protect } = require('../middleware/authMiddleware')
 const { setBook, getBooks, getBookByParams } = require('../controllers/bookController')
 
-router.route('/').get(getBooks).post(setBook) // Set Book | Get Books
-router.route('/:params').get(getBookByParams) // Get Detail Book
+router.route('/').get(semiProtected, getBooks).post(protect, setBook) // Set Book | Get Books
+router.route('/:params').get(semiProtected, getBookByParams) // Get Detail Book
 
 module.exports = router
