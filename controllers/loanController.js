247b9b8055bd54ac644d5e7171e8dca5df16e6bf
bukanrayhan <rayhan.rhssoe2@gmail.com>
2022-07-14 10:59:02 +0700

    getMyLoans and completeTheLoan functions.

--- controllers/loanController.js
+++ controllers/loanController.js
@@ -125,8 +125,105 @@ const getLoanById = asyncHandler(async (req, res) => {
 	}
 })
 
+// @desc Read My Loan
+// @route GET /api/loans/myloans
+// @access PRIVATE
+const getMyLoans = asyncHandler(async (req, res) => {
+	// get queries
+	let { isActive, isCompleted, isForced } = req.query
+
+	// convert string to boolean
+	isActive = isActive === 'true'
+	isCompleted = isCompleted === 'true'
+	isForced = isForced === 'true'
+
+	// GET LOANS
+	const loans = await ((isActive && isCompleted && isForced) || // is READ all loans ?
+	(!isActive && !isCompleted && !isForced) || // is READ all loans ?
+	(isActive && isCompleted) // is READ all loans ?
+		? // then show all loans
+		  Loan.find({ user: req.user._id })
+		: // check completed loans
+		isCompleted
+		? // then show completed loans
+		  Loan.find({ user: req.user._id, isCompleted })
+		: // check active & forced loans
+		isActive && isForced
+		? // then show active & forced loans
+		  Loan.find({ user: req.user._id, $or: [{ isCompleted: !isActive }, { isForced }] })
+		: // check active loans
+		isActive
+		? // then show active loans
+		  Loan.find({ user: req.user._id, isCompleted: !isActive })
+		: // the show forced loans
+		  Loan.find({ isForced })
+	)
+		.populate('user', 'name') // for public just show the code
+		.populate('book', 'title') // for public just show the code
+		.select({ message: 0, createdAt: 0, updatedAt: 0 }) // selecting the field for response
+		.lean()
+
+	// status code & response
+	res.status(200).json({
+		loans,
+		total: loans.length,
+	})
+})
+
+// @desc Update My Loan
+// @route PUT /api/loans/:id
+// @access PRIVATE
+const completeTheLoan = asyncHandler(async (req, res) => {
+	try {
+		let loan = await Loan.findById(req.params?.id)
+		let user = await User.findById(req?.user?._id)
+		let book = await Book.findById(loan?.book)
+
+		if (!loan || !user || !book) {
+			res.status(424)
+			throw new Error('Failed to load this loan data.')
+		}
+
+		// update loan data
+		loan.isCompleted = true
+		loan.completedAt = new Date()
+		loan.message.push(`${book.title} book has been returned by ${user.name} on ${new Date()}`)
+		await loan.save()
+
+		// update user
+		const newBooks = user.books.filter((id) => JSON.stringify(id) !== JSON.stringify(book._id))
+		user.books = newBooks
+		await user.save()
+
+		// update user
+		book.stock = 1
+		book.user = null
+		await book.save()
+
+		loan = await Loan.findById(loan._id)
+			.select({ _id: 1, isCompleted: 1, message: 1, completedAt: 1 })
+			.lean()
+		user = await User.findById(user?._id)
+			.populate('books', 'title code')
+			.select({ _id: 1, name: 1, code: 1, books: 1 })
+			.lean()
+		book = await Book.findById(book?._id).select({ _id: 1, code: 1, title: 1, stock: 1 }).lean()
+
+		res.status(200).json({
+			loan,
+			book,
+			user,
+		})
+	} catch (error) {
+		res.status(424)
+		throw new Error(error)
+	}
+})
+
 module.exports = {
 	setLoan,
 	getLoans,
 	getLoanById,
+	getMyLoans,
+	completeTheLoan,
 }
